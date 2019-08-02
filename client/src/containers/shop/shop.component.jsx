5884f1e5b762063fdca26fe482e3b6c43f40f660
Fernando Faria <fernandofaria@Fernandos-MacBook-Air.local>
2019-08-01 22:33:13 -0400

    testing phpstorm

--- client/src/containers/shop/shop.component.jsx
+++ client/src/containers/shop/shop.component.jsx
@@ -1,31 +1,36 @@
-import React, { Component } from 'react'
-import {SHOP_DATA} from './shop.data'
-import PreviewItem from '../../components/shopPreviewItem/preview-item.component';
-import './shop.styles.scss'
+import React, { Component } from "react";
+import { SHOP_DATA } from "./shop.data";
+import PreviewItem from "../../components/shopPreviewItem/preview-item.component";
+import "./shop.styles.scss";
 export default class Shop extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    
+
     this.state = {
       collections: SHOP_DATA
-    }
+    };
   }
   render() {
-   
     return (
-      <div>
-      <h1 className="title">My Shop</h1>
-      <div className="flex-container">
-      
-      {this.state.collections[`${this.props.match.params.id - 1}`].items.map(item => {
-        return (
-          <PreviewItem key={item.id} name={item.name} price={item.price} imageUrl={item.imageUrl}  />
-        )
-      })}
-
+      <div className="container" style={{ marginTop: "50px" }}>
+        <h1 className="title">
+          {this.state.collections[this.props.match.params.id - 1].title}
+        </h1>
+        <div className="flex-container">
+          {this.state.collections[
+            `${this.props.match.params.id - 1}`
+          ].items.map(item => {
+            return (
+              <PreviewItem
+                key={item.id}
+                name={item.name}
+                price={item.price}
+                imageUrl={item.imageUrl}
+              />
+            );
+          })}
+        </div>
       </div>
-        
-      </div>
-    )
+    );
   }
 }
